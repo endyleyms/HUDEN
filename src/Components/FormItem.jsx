4f390bfa45050b94bac45d814b84c0423baedc71
Wendy Medina <endyleyms@gmail.com>
2024-01-29 22:31:46 -0500

    input data

--- src/Components/FormItem.jsx
+++ src/Components/FormItem.jsx
@@ -1,17 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-export const FormItem = ({title, value, setValue}) => {
-  // const [value, setValue]=useState(data);
-  // const handleChange = (e) => {
-  //   const newValue = e.target.value;
-  //   setValue(newValue);// Llama a handleInput con el nuevo valor
-  // };
+export const FormItem = ({title, data, value, setValue}) => {
 
   return (
     <div>
       <span className="text-center">{title}</span>
       <form class="form">
-        <input type="text" class="form-control" id="floatingInputInvalid" value={value} onChange={e=>setValue(e.target.value)}/>
+        <input type="text" class="form-control" id="floatingInputInvalid" value={data ? value || data : value} onChange={e=>setValue(e.target.value)}/>
         {/* <label for="floatingInputInvalid">{placeholder}</label> */}
       </form>
     </div>
