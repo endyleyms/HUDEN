a0ac71d2d37b9bda6f0d7214af5bbeff5f878aae
Wendy Medina <endyleyms@gmail.com>
2024-02-06 15:44:30 -0500

    fixing data

--- src/Components/ResumePedido.jsx
+++ src/Components/ResumePedido.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import TableResume from './TableResume'
 
-function ResumePedido({handleShow, selecData}) {
+function ResumePedido({selecData}) {
   return (
     <div>
       <div className="d-flex justify-content-center align-items-center">
         <div className="modal-content">
-          {/* <div className="modal-header">
-            <h1 className="modal-title fs-5" id="exampleModalLabel">Resumen del pedido</h1>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={handleShow}></button>
-          </div> */}
           <div style={{width: '90%'}}>
             <TableResume data={selecData}/>
           </div>
